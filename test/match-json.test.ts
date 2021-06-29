0beaf8ac60f79348bc474c8ffe1db333d24012a7
William Swanson <swansontec@gmail.com>
2021-06-29 09:42:54 -0700

    Add the missing `matchJson` unit test
    This was supposed to go in the william/couchdb-tools PR, but I forgot to `git add` it.

---
+++ test/match-json.test.ts
@@ -0,0 +1,30 @@
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+import { matchJson } from '../src/util/match-json'
+
+describe('matchJson', function () {
+  it('matches similar JSON structures', function () {
+    expect(
+      matchJson(
+        { object: { a: 1, b: 'string' }, array: [false, null] },
+        { array: [false, null], object: { b: 'string', a: 1 } }
+      )
+    ).equals(true)
+  })
+
+  it('rejects unequal JSON arrays', function () {
+    expect(matchJson([1, 2], [1, 2, 3])).equals(false)
+    expect(matchJson([1, 2], [1, 3])).equals(false)
+    expect(matchJson([1, 2], { 1: 1, 2: 2 })).equals(false)
+    expect(matchJson({ 1: 1, 2: 2 }, [1, 2])).equals(false)
+    expect(matchJson([1, 2], null)).equals(false)
+  })
+
+  it('rejects unequal JSON objects', function () {
+    expect(matchJson({ a: 1, b: 2 }, { a: 1 })).equals(false)
+    expect(matchJson({ a: 1 }, { a: 1, b: 2 })).equals(false)
+    expect(matchJson({ a: 1, b: 2 }, { a: 1, b: 3 })).equals(false)
+    expect(matchJson({ a: 1, b: 2 }, null)).equals(false)
+  })
+})
