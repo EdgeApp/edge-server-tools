7b95e1f28af3ce623454f7e385c6af7bc6519324
William Swanson <swansontec@gmail.com>
2021-09-20 17:04:02 -0700

    Remove dummy test
    This is no longer necessary, since we have a variety of real tests now.

--- test/dummy.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-import { describe, it } from 'mocha'
-
-describe('example test', function () {
-  it('should do something', function () {
-    expect(1 + 2).equals(3)
-  })
-})
