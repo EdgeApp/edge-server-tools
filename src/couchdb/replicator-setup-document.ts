defc5216ecf951a825c66d2e64be261d3d4bc56d
William Swanson <swansontec@gmail.com>
2022-06-09 16:21:47 -0700

    Heal broken replicator setups

--- src/couchdb/replicator-setup-document.ts
+++ src/couchdb/replicator-setup-document.ts
@@ -1,14 +1,27 @@
-import {
-  asArray,
-  asMaybe,
-  asObject,
-  asOptional,
-  asString,
-  asValue,
-  Cleaner
-} from 'cleaners'
+import { asArray, asOptional, asString, asValue } from 'cleaners'
 
 import { asHealingObject } from '../cleaners/as-healing-object'
+
+interface ClusterRow {
+  // The base URI to connect to:
+  url: string
+
+  // The base64 "username:password" pair used to authenticate.
+  // To get this, run `btoa('username:password')` in a JS console:
+  basicAuth?: string
+
+  // Database names to replicate with this cluster.
+  // Use an ending '*' for wildcard matching.
+  // The `exclude` list defaults to `[]`.
+  // The `include` list defaults to `['*']`.
+  exclude?: string[]
+  include?: string[]
+
+  // How the replications should take place.
+  // If a replicator is a "source", its peers will pull changes from it.
+  // If a replicator is a "target", its peers will push their changes to it.
+  mode: 'source' | 'target' | 'both'
+}
 
 /**
  * A list of nodes to use for replication,
@@ -16,41 +29,34 @@ import { asHealingObject } from '../cleaners/as-healing-object'
  */
 export interface ReplicatorSetupDocument {
   clusters: {
-    [clusterName: string]: {
-      // The base URI to connect to:
-      url: string
-
-      // The base64 "username:password" pair used to authenticate.
-      // To get this, run `btoa('username:password')` in a JS console:
-      basicAuth?: string
-
-      // Database names to replicate with this cluster.
-      // Leave the list missing to skip filtering, and use an ending '*'
-      // to treat an entry as a prefix instead of an exact name.
-      exclude?: string[]
-      include?: string[]
-
-      // How the replications should take place.
-      // If a replicator is a "source", its peers will pull changes from it.
-      // If a replicator is a "target", its peers will push their changes to it.
-      mode: 'source' | 'target' | 'both'
-    }
+    [clusterName: string]: ClusterRow
   }
 }
+
+const asClusterRow = asHealingObject<ClusterRow>(
+  {
+    url: asString,
+    basicAuth: asString,
+    exclude: asOptional(asArray(asString)),
+    include: asOptional(asArray(asString)),
+    mode: asValue('source', 'target', 'both')
+  },
+  {
+    url: '',
+    mode: 'source'
+  }
+)
 
 /**
  * Validates a replicator setup document.
  * Handles errors gracefully for use with with `syncedDocument`.
  */
-export const asReplicatorSetupDocument: Cleaner<ReplicatorSetupDocument> =
-  asObject({
-    clusters: asHealingObject(
-      asObject({
-        url: asMaybe(asString, ''),
-        basicAuth: asMaybe(asString),
-        exclude: asMaybe(asOptional(asArray(asString))),
-        include: asMaybe(asOptional(asArray(asString))),
-        mode: asMaybe(asValue('source', 'target', 'both'), 'source')
-      })
-    )
-  })
+export const asReplicatorSetupDocument =
+  asHealingObject<ReplicatorSetupDocument>(
+    {
+      clusters: asHealingObject(asClusterRow)
+    },
+    {
+      clusters: {}
+    }
+  )
